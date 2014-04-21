0717b98be4d2e6e18c9ebd5416a84fff496aefde
haoranliu <liuhaoran@172-26-43-16.dynapool.nyu.edu>
2014-04-21 14:12:47 -0400

    UI...

--- src/com/example/locationremindersv0/MainActivity.java
+++ src/com/example/locationremindersv0/MainActivity.java
@@ -4,6 +4,7 @@ package com.example.locationremindersv0;
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Color;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
